cf65d0fefeb148dadf8d4d0a5519b5b000c9981c
Jwsoat <34992188+jwsoat@users.noreply.github.com>
2021-02-09 16:09:19 +1300

    0.0.2

--- maths.py
+++ maths.py
@@ -8,6 +8,12 @@ if math == 1:
 if math == 2:
     minusans = math1 - math2
     print("Answer is:", minusans)
+if math == 3:
+    timesans = math1 * math2
+    print(timesans)
+if math == 4:
+    divideans = math1 / math2
+    print(divideans)
 
 
 print("©2021 Jwsoat Media")
