3c5731131a389ed0355c611858b214b35a1a4a0a
Jwsoat <34992188+jwsoat@users.noreply.github.com>
2021-02-09 16:04:46 +1300

    0.0.1

--- main.py
+++
@@ -1,6 +0,0 @@
-hi = int(input("1+1 "))
-if hi == 2:
-    print(hi)
-else:
-    print("your stupid")
-
---
+++ maths.py
@@ -0,0 +1,13 @@
+math1 = int(input("First Number? "))
+math2 = int(input("Second Number? "))
+math = int(input("1= Plus 2= Minus 3= times 4=divide "))
+
+if math == 1:
+    plusans= math1 + math2
+    print (plusans)
+if math == 2:
+    minusans = math1 - math2
+    print("Answer is:", minusans)
+
+
+print("©2021 Jwsoat Media")
