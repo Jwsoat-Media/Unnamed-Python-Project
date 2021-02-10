f334beed6d46781990043d7db5d5ea2448c289c4
Jwsoat <34992188+jwsoat@users.noreply.github.com>
2021-02-10 21:14:32 +1300

    0.0.6

---
+++ allscripts.py
@@ -0,0 +1,4 @@
+import infcounter
+selector = int(input("Choose your game 1 = infcounter"))
+if selector = 1:
+    infcounter
---
+++ guessthenumber.py
@@ -0,0 +1,6 @@
+import math
+import random
+low = int(input("Pick a number"))
+high = int(input("Pick a another number"))
+print(random.randrange(low,high))
+if 
